6d42705a8551b05362f41df0f9a1d39acee10e2d
Joseph Sinclair <joey.sinclair02@gmail.com>
2020-04-15 21:33:24 -0700

    changes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 import json, os
-import tools, text
+import tools
 
 app = Flask(__name__)
 port = int(os.environ.get('PORT', 5000))
