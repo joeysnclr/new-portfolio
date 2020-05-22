879d984510f427362d7c9ac3e87b4a8d57c76cce
Joseph Sinclair <joey.sinclair02@gmail.com>
2020-05-22 15:43:53 -0700

    almost done! projects display, resume, contact, added projects

--- app.py
+++ app.py
@@ -13,12 +13,4 @@ def index():
     return render_template('index.html', projects=tools.getProjects())
 
 
-@app.route('/project/<string:projectId>')
-def project(projectId):
-    p = tools.getProject(projectId)
-    if p == None:
-        return redirect(url_for("index"))
-    return render_template('project.html', pId=projectId, p=p)
-
-
 app.run(host='0.0.0.0', port=port, debug=True)
--- tools.py
+++ tools.py
@@ -1,9 +1,7 @@
 import json
 
-def getProjects():
-	with open("./static/data/projects.json", "r+") as file:
-		data = json.load(file)
-	return data
 
-def getProject(projId):
-	return getProjects().get(projId)
+def getProjects():
+    with open("./static/data/projects.json", "r+") as file:
+        data = json.load(file)
+    return data
